a3e447abd563b7bf8857893f0adcf39f7b5af4e9
james miller <jamesstewartmiller@gmail.com>
2022-08-11 16:56:17 +0100

    debugging

--- scripts/image_acq.sh
+++ scripts/image_acq.sh
@@ -5,7 +5,7 @@ source ${PROJECT_SETTINGS}
 for image_src in $(find ${CONTAINER_SCRIPTS_ROOT}/containers -type f -name "source.sh" | sort)
 do
     source ${image_src}
-    if [[ -n "${USER_NAME}" ]];
+    if [[ -n "${USER_NAME}" ]];   # TODO define a project settings options file that states whether the project is std user or super user account
     then
         if ! runuser ${USER_NAME} -l -c "podman image exists ${TAG}";
         then
